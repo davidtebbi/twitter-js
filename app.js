1c683c0107f15c76e26be9c19d1e36714eed5715
David Tebbi <davidtebbi@gmail.com>
2015-05-16 10:25:45 -0400

    add bootstrap template

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var morgan = require('morgan');
 var app = express();
+var swig = require('swig');
+var routes = require('./routes/');
+
+
+app.use('/', routes);
 
 app.use(morgan('dev'));
+
+//tell express what templating system we're using. engine method takes two arguments, the view engine, html usually,
+// and the rendering function of the templating engine. the html lets express know the file extension, so we don't have to include it below
+app.engine('html', swig.renderFile);
+
+//you also have to set the view engine for express and the templating engine
+app.set('view engine', 'html');
+
+app.set('views', __dirname + "/views");
+
+app.use(express.static(__dirname + '/public'));
+
+swig.setDefaults({ cache: false });
 
 var server = app.listen(3000, function () {
 
@@ -11,8 +29,4 @@ var server = app.listen(3000, function () {
 
   console.log('server listening', host, port);
 
-});
-
-app.get('/', function (req, res) {
-  res.send('Hi!');
 });
